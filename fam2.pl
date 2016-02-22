44611d729832c8b2190d58269ed601f0f0d9802f
Kamil Kryjak <kamikryj@borg.kis.agh.edu.pl>
2016-02-22 12:12:36 +0100

    Family relations. Lab 1

--- fam2.pl
+++ fam2.pl
@@ -1,8 +1,10 @@
 rodzic(kasia,robert).
 rodzic(tomek,robert).
 rodzic(tomek,eliza).
+rodzic(henryk,krystyna).
 rodzic(kasia, eliza).
 rodzic(robert,anna).
+rodzic(krystyna,anna).
 rodzic(robert,magda).
 rodzic(magda,jan).
  
@@ -10,10 +12,12 @@ kobieta(kasia).
 kobieta(eliza).
 kobieta(magda).
 kobieta(anna).
+kobieta(krystyna).
  
 mezczyzna(tomek).
 mezczyzna(robert).
 mezczyzna(jan).
+mezczyzna(henryk).
 
 famme(kasia).
 homme(krzys).
@@ -26,4 +30,40 @@ ojciec(X,Y) :-
 	rodzic(X,Y), mezczyzna(X).
 
 brat(X,Y) :-
-	matka(Z,X),matka(Z,Y),X\=Y,ojciec(F,X),ojciec(F,Y),mezczyzna(X).
+	matka(Z,X),
+	matka(Z,Y),
+	X\=Y,
+	ojciec(F,X),
+	ojciec(F,Y),
+	mezczyzna(X).
+
+siostra(X,Y) :-
+	matka(Z,X),
+	matka(Z,Y),
+	X\=Y,
+	ojciec(F,X),
+	ojciec(F,Y),
+	kobieta(X).
+
+dziadek(X,Y) :-
+	rodzic(F,Y),
+	ojciec(X,F).
+
+babcia(X,Y) :-
+	rodzic(M,Y),
+	matka(X,M).
+
+przodek(X,Y) :- 
+	rodzic(X,Y).
+
+przodek(X,Z) :-
+	rodzic(X,Y),
+	przodek(Y,Z).
+
+potomek(X,Y) :-
+	rodzic(Y,X).
+
+potomek(X,Y) :-
+	rodzic(Z,X),
+	potomek(Z,Y).
+	
